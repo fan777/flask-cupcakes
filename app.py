55c8b473e97b931b18f6737c4cd2f42934b2532e
fan777 <fan777@gmail.com>
2021-04-09 19:35:01 -0700

    basic assignment

--- app.py
+++ app.py
@@ -1,1 +1,69 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, request, render_template, jsonify
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "asdfhl45lhasdf"
+
+connect_db(app)
+db.create_all()
+
+
+@app.route('/')
+def index_page():
+    return render_template('index.html')
+
+
+# **********************************
+# RESTFUL CUPCAKES JSON API
+# **********************************
+
+
+@app.route('/api/cupcakes')
+def list_cupcakes():
+    '''Returns JSON w/ all cupcakes'''
+    all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=all_cupcakes)
+
+
+@app.route('/api/cupcakes/<int:id>')
+def get_cupcake(id):
+    '''Returns JSON for one cupcake'''
+    cupcake = Cupcake.query.get_or_404(id)
+    return jsonify(cupcake=cupcake.serialize())
+
+
+@app.route('/api/cupcakes', methods=['POST'])
+def create_cupcake():
+    '''Creates a new cupcake and returns JSON of that created cupcake'''
+    new_cupcake = Cupcake(flavor=request.json['flavor'], size=request.json['size'], rating=request.json[
+                          'rating'], image=request.json['image'])
+    db.session.add(new_cupcake)
+    db.session.commit()
+    response_json = jsonify(cupcake=new_cupcake.serialize())
+    return (response_json, 201)
+
+
+@app.route('/api/cupcakes/<int:id>', methods=['PATCH'])
+def update_cupcake(id):
+    '''Updates a cupcake and responds w/ JSON of that created cupcake'''
+    cupcake = Cupcake.query.get_or_404(id)
+    cupcake.flavor = request.json.get('flavor', cupcake.flavor)
+    cupcake.size = request.json.get('size', cupcake.size)
+    cupcake.rating = request.json.get('rating', cupcake.rating)
+    cupcake.image = request.json.get('image', cupcake.image)
+    db.session.commit()
+    return jsonify(cupcake=cupcake.serialize())
+
+
+@app.route('/api/cupcakes/<int:id>', methods=['DELETE'])
+def delete_cupcake(id):
+    '''Deletes a cupcake'''
+    cupcake = Cupcake.query.get_or_404(id)
+    db.session.delete(cupcake)
+    db.session.commit()
+    return jsonify(message='deleted')
